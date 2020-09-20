06757a8a256523a5831334c0b076ad678123d33a
Vitaliy Lekay <vlekay@rivne.itstep.org>
2020-09-20 13:53:41 +0300

    covid db input

--- lib/Settings.py
+++ lib/Settings.py
@@ -6,6 +6,6 @@ URL = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5"
 URL_COVID = "https://api.covid19api.com/summary"
 
 # Database configuration
-HOSTNAME = "35.239.38.98"
+HOSTNAME = "34.68.62.240"
 USER = "root"
 PASSWORD = "password123"
